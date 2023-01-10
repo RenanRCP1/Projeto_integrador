9506bb322632109e77f2fa30c61ac30d8c91c0f6
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-01-09 22:11:08 -0300

    login

---
+++ src/components/login/InputSenhaLogin.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="q-pa-md bg-orange-4">
+        <div class="q-gutter-md ">
+            <q-input class="bg-white" color="primary" v-model="password" :type="isPwd ? 'password' : 'text'"
+                label="Senha" filled>
+                <template v-slot:append>
+                    <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                        @click="isPwd = !isPwd" />
+                </template>
+            </q-input>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+
+export default {
+    setup() {
+        return {
+            password: '',
+            isPwd: true,
+
+
+        }
+    }
+}
+</script>
+
+
